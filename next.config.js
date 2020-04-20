c230a0d062482bbe17301f411ae7758bbe645c52
Sam Garson <samtgarson@gmail.com>
2020-04-20 21:43:17 +0000

    Add compose-plugins and sourcemaps plugin

--- next.config.js
+++ next.config.js
@@ -1,11 +1,19 @@
+/* eslint-disable import/no-extraneous-dependencies, @typescript-eslint/no-var-requires */
 if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 const path = require('path')
+const withPlugins = require('next-compose-plugins')
+const sourcemaps = require('@zeit/next-source-maps')
 
 const { PUSHER_KEY, PUSHER_CLUSTER } = process.env
-module.exports = {
-  webpack: config => {
-    config.resolve.alias['~'] = path.resolve(__dirname) + "/"
-    return config
-  },
-  env: { PUSHER_CLUSTER, PUSHER_KEY }
-}
+module.exports = withPlugins(
+  [
+    [sourcemaps]
+  ],
+  {
+    webpack: config => {
+      config.resolve.alias['~'] = path.resolve(__dirname) + "/"
+      return config
+    },
+    env: { PUSHER_CLUSTER, PUSHER_KEY }
+  }
+)
