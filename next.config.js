ab09cdbae1336dc225e29899fb0f11e2babd553b
Sam Garson <samtgarson@gmail.com>
2021-03-31 21:30:33 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,20 +4,8 @@ const path = require('path')
 const withPlugins = require('next-compose-plugins')
 const sourcemaps = require('@zeit/next-source-maps')()
 
-const { PUSHER_KEY, PUSHER_CLUSTER } = process.env
-
 module.exports = withPlugins(
   [
     [sourcemaps]
-  ],
-  {
-    webpack (config) {
-      config.resolve.alias['~'] = path.resolve(__dirname) + "/"
-      return config
-    },
-    env: {
-      PUSHER_CLUSTER,
-      PUSHER_KEY
-    }
-  }
+  ]
 )
