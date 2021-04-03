5909fd9ee6d79e33cc22a708bde9d88f6f56d4c4
Sam Garson <samtgarson@gmail.com>
2021-04-03 19:52:48 +0100

    Add content to login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,15 +1,33 @@
 import { GetServerSideProps, NextPage } from "next"
-import { useEffect } from "react"
+import { useRouter } from "next/router"
+import { Button } from "rbx"
+import React from "react"
+import { DobbleTitle } from "../components/title"
+import { Wrapper } from "../components/wrapper"
 import { DataClient } from "../services/data-client"
+import { GlobalState } from "../services/state"
+
+const googleIcon = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/200px-Google_%22G%22_Logo.svg.png"
 
 const LogIn: NextPage<{ redirect: string}> = ({ redirect }) => {
+  const { user: { auth_id: loggedIn } = {} } = GlobalState.useContainer()
   const client = DataClient.useClient()
+  const router = useRouter()
 
-  useEffect(() => {
-    client.auth.signIn({ provider: 'google' }, { redirectTo: new URL(`/auth?redirect=${redirect}`, location.href).toString() })
-  })
+  const login = () => client.auth.signIn({ provider: 'google' }, { redirectTo: new URL(`/auth?redirect=${redirect}`, location.href).toString() })
 
-  return null
+  if (loggedIn) {
+    router.push('/')
+    return null
+  }
+
+  return (
+    <Wrapper>
+      <DobbleTitle text='Sign In' />
+      <p style={{ marginBottom: 20 }}>Use your Google account to sign into Dobble</p>
+      <Button size="large" onClick={login}><img src={googleIcon} style={{ height: '1em', marginRight: 10 }} /> Continue with Google</Button>
+    </Wrapper>
+  )
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ query: { redirect = '/' } }) => ({ props: { redirect } })
